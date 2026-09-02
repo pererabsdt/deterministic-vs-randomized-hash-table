df4b50a1f5d270c825c149736c35ae7d86f212d1
himalagunathilaka <himala.22@cse.mrt.ac.lk>
2026-09-02 21:28:04 +0530

    refractor: Add std::

--- src/data/DataGenerator.cpp
+++ src/data/DataGenerator.cpp
@@ -1,19 +1,18 @@
 #include "data/DataGenerator.h"
 #include <random>
 #include <string>
-using namespace std;
 
-vector<string> DataGenerator::generateBenignData(size_t size, size_t length) {
-    vector<string> dataset;
+std::vector<std::string> DataGenerator::generateBenignData(size_t size, size_t length) {
+    std::vector<std::string> dataset;
     // Random alphanumeric string generation
     
-    random_device rd;
-    mt19937 generator(rd());
+    std::random_device rd;
+    std::mt19937 generator(rd());
     
-    uniform_int_distribution<> distribution(0, CHARACTERS.size() -1);
+    std::uniform_int_distribution<> distribution(0, CHARACTERS.size() -1);
     
     for(size_t i = 0 ; i< size;i++){
-        string random_string;
+        std::string random_string;
         random_string.reserve(length);
         for (size_t j = 0; j < length; ++j) {
             random_string += CHARACTERS[distribution(generator)];
@@ -23,8 +22,8 @@ vector<string> DataGenerator::generateBenignData(size_t size, size_t length) {
     return dataset;
 }
 
-vector<string> DataGenerator::generateAdversarialData(size_t numPairs) {
-    vector<string> dataset;
+ std::vector<std::string> DataGenerator::generateAdversarialData(size_t numPairs) {
+    std::vector<std::string> dataset;
     // TODO: Implement Meet-in-the-Middle combinatorial string generation
     return dataset;
 }
